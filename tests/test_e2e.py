a9ed692ba6341c1962387f57501abf87e3a614cb
Yundo <nohoho37@hanmail.net>
2025-11-04 15:42:07 +0900

    American development team

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -26,6 +26,10 @@ class TestOne(BaseClass):
                 print("develop1")  # 깃 실습용
                 print("develop2")  # 깃 실습용
                 print("develop3")  # 깃 실습용
+
+                print("GitX1")  # 깃 실습용
+                print("GitX2")  # 깃 실습용
+                print("GitX3")  # 깃 실습용
                 break
 
         self.driver.find_element(By.CSS_SELECTOR, "a[class*=btn-primary]").click()
